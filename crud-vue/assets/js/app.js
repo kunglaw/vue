d3d78dcac704b208aaf2432d577c99f39a4bc866
Kunglaw <alhusna901@gmail.com>
2018-11-05 00:29:54 +0700

    idstack and vue-crud

---
+++ crud-vue/assets/js/app.js
@@ -0,0 +1,33 @@
+var app = new Vue({
+    el:"#app",
+    data:{
+        title:"",
+        price:0,
+        description:"",
+        products:[
+            {title:"Asus ROG RAM 16GB",
+            price:15000000,
+            description:"Ajegile ini computer gaming"},
+            {title:"Corsair RAM 16GB",
+            price:2000000,
+            description:"keren cuy"},
+            {title:"Samsung Harddisk 1TB",
+            price:1500000,
+            description:"nyimpen berapa anime tuuh ?"},
+        ]
+
+    },
+    computed:{
+        
+    },
+    methods:{
+        add_product:function(){
+            alert()
+            this.products.push({
+                title:this.title,
+                price:this.price,
+                description:this.description
+            })
+        }
+    }
+})
